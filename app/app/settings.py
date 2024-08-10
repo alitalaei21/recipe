3c317b7301a742670da83e86dfd82e5bc0cd2dca
alitalaei21 <alitalayi21@gmail.com>
2024-08-10 18:06:51 +0330

    kim888888888

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,12 +137,3 @@ STATIC_ROOT = '/vol/web/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'core.User'
-
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-}
-
-SPECTACULAR_SETTINGS = {
-    'COMPONENT_SPLIT_REQUEST': True,
-}
