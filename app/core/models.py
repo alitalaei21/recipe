9a93edd4ec29052ca76355f54bdea56843d8cef4
alitalaei21 <alitalayi21@gmail.com>
2024-08-18 19:32:16 +0330

    update proj

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
-
 class UserManager(BaseUserManager):
     def create_user(self,email,password=None,**extra_fields):
         if not email :
@@ -12,12 +11,7 @@ class UserManager(BaseUserManager):
         user=self.model(email=self.normalize_email(email),**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
-
-
-
-
 class User(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(max_length=255,unique=True)
     name=models.CharField(max_length=255)
